697a13cc5bfb8b42c6ecc377ccb7914a6e381b07
Paul Nguyen <PaulNguyen@Matthews-MacBook-Air.local>
2020-12-14 11:25:41 -0600

    connected api

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var queryURL = "api.openweathermap.org/data/2.5/forecast?id=524901&appid=225a815d34855ab38313165a24d26606"
+const apiKey = "cfab7f3712be5d9f10bb02b720dcb30c"
+const api = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid="
 
 $.ajax({
-    url: queryURL,
+    url: api + apiKey,
     method: "GET"
 }).then(function(response) {
     console.log(response)
