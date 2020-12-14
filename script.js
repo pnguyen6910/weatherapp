5483b8c9212b0cd8d922a2bbc18a308a732bd834
Paul Nguyen <PaulNguyen@Matthews-MacBook-Air.local>
2020-12-14 15:48:24 -0600

    quick update

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// ***** Global Variablies *****
 const apiKey = "cfab7f3712be5d9f10bb02b720dcb30c"
-const api = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid="
+const api = "http://api.openweathermap.org/data/2.5/forecast?id=4887398&units=imperial&appid="
+const fiveDayApi = "http://api.openweathermap.org/data/2.5/forecast?q={city name}"
 
+
+
+//****** AJAX Call *****
 $.ajax({
     url: api + apiKey,
     method: "GET"
 }).then(function(response) {
+    console.log(response)
+    console.log(response.list[0].main)
+    console.log(response.city.name)
+    console.log(response)
     console.log(response)
 })
