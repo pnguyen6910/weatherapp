390b30a35b4430ccc27dfd35d5a8f64bec78022e
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-03-18 11:41:27 -0500

    html & css updated

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 // ***** Global Variablies *****
+const searchCity = $("#search-city");
+const searchButton = $("#search-button");
+const clearButton = $("#clear-button");
+const presentCity = $("#present-city");
+const presentTemperature = $("#temperature");
+const humidty = $("#humidity");
+const windSpeed = $("#wind-speed");
+const uvIndexList = $("#uv-index");
 const apiKey = "cfab7f3712be5d9f10bb02b720dcb30c"
 const api = "http://api.openweathermap.org/data/2.5/forecast?id=4887398&units=imperial&appid="
-const fiveDayApi = "http://api.openweathermap.org/data/2.5/forecast?q={city name}"
+const list = $(".list-group")
 
+var cityArray = JSON.parse(localStorage.getItem("cityname")) || []
+var city = "";
+var d = new Date();
+var year = d.getFullYear()
+var month = d.getMonth() + 1;
+var day = d.getDate();
+var date = day + "." + month + "." + year
 
-
-//****** AJAX Call *****
-$.ajax({
-    url: api + apiKey,
-    method: "GET"
-}).then(function(response) {
-    console.log(response)
-    console.log(response.list[0].main)
-    console.log(response.city.name)
-    console.log(response)
-    console.log(response)
-})
+function removeDupe() {
+    cityArraydupe = cityArray.filter(
+        function (a) {
+            if (!this[a]) {
+                this[a] = 1
+                return a
+            }
+        }, {}
+    )
+    cityArray = cityArraydupe
+    localStorage.setItem('cityname', JSON.stringify(cityArray))
+    list.empty()
+}
